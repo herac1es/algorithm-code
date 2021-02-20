f396125484e619ace966bffc0d4de1dab8a2de2a
malishen <malishen@inke.cn>
2021-02-20 11:53:59 +0800

    121.买卖股票的最佳时机

---
+++ leetcode/q121.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func maxProfit(prices []int) int {
+	n := len(prices)
+	dp := make([][2]int, n)
+	for i := 0; i < n; i++ {
+		if i == 0 {
+			dp[i][0] = 0
+			dp[i][1] = -prices[i]
+			continue
+		}
+		dp[i][0] = max(dp[i-1][0],dp[i-1][1]+prices[i])
+		dp[i][1] = max(-prices[i],dp[i-1][1])
+	}
+	return dp[n-1][0]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
