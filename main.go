ddbfbbd4c9e6027bbff77547c8f3359786f5fb1c
malishen <malishen@inke.cn>
2021-02-28 19:32:14 +0800

    无重叠区间

--- main.go
+++ main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/iwillbesober/algorithm-code/leetcode"
+	"sort"
 )
 
 func main() {
-	fmt.Println(leetcode.MinDistance("ab", "ab"))
+	inters := [][]int{{1, 2}, {2, 3}, {3, 4}, {1, 3}}
+	sort.Slice(inters, func(i, j int) bool {
+		return inters[i][1] < inters[j][1]
+	})
+	x := inters[0]
+	res := 1
+	for i := 1; i < len(inters); i++ {
+		if inters[i][0] >= x[1] {
+			x = inters[i]
+			res++
+		}
+	}
+	fmt.Println(len(inters) - res)
 }
