428b9a95205e16ad49fbc781531caf7d2d6e7221
malishen <malishen@inke.cn>
2021-03-01 17:54:48 +0800

    198大家结社

--- leetcode/q673.go
+++ leetcode/q673.go
@@ -1,5 +1,25 @@
 package leetcode
 
 func FindNumberOfLIS(nums []int) int {
-	return 0
+	n := len(nums)
+	dp := make([]int, n)
+	res := make([]int, n+1)
+	for i := range dp {
+		dp[i] = 1
+		res[i]++
+	}
+	max := dp[0]
+	for i := 1; i < n; i++ {
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				val := dp[j] + 1
+				if val > max {
+					max = val
+					dp[i] = val
+					res[val] = res[val-1] + 1
+				}
+			}
+		}
+	}
+	return res[n]
 }
