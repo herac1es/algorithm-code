3dbee0219f86998ff2303b3167516cb0f2d0599b
malishen <malishen@inke.cn>
2021-03-03 15:23:13 +0800

    213打家劫舍II

--- main.go
+++ main.go
@@ -2,21 +2,11 @@ package main
 
 import (
 	"fmt"
-	"sort"
+
+	"github.com/iwillbesober/algorithm-code/leetcode"
 )
 
 func main() {
-	inters := [][]int{{1, 2}, {2, 3}, {3, 4}, {1, 3}}
-	sort.Slice(inters, func(i, j int) bool {
-		return inters[i][1] < inters[j][1]
-	})
-	x := inters[0]
-	res := 1
-	for i := 1; i < len(inters); i++ {
-		if inters[i][0] >= x[1] {
-			x = inters[i]
-			res++
-		}
-	}
-	fmt.Println(len(inters) - res)
+	in := []int{2, 3, 2}
+	fmt.Println(leetcode.Rob(in))
 }
