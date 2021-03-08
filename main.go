6e25b4eae3469ed0d5aad29ef9533bbf2b8f3f4a
malishen <malishen@inke.cn>
2021-03-09 01:10:22 +0800

    11 装水最多的容器

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/iwillbesober/algorithm-code/leetcode"
+import (
+	"fmt"
+
+	"github.com/iwillbesober/algorithm-code/leetcode"
+)
 
 // 测试用例:["LRUCache","put","put","put","put","get","get","get","get","put","get","get","get","get","get"]
 //				[[3],[1,1],[2,2],[3,3],[4,4],[4],[3],[2],[1],[5,5],[1],[2],[3],[4],[5]]
@@ -8,19 +12,6 @@ import "github.com/iwillbesober/algorithm-code/leetcode"
 //				期望结果:[null,null,null,null,null,4,3,2,-1,null,-1,2,3,-1,5]
 //				stdout:
 func main() {
-	cache := leetcode.Constructor(3)
-	cache.Put(1, 1)
-	cache.Put(2, 2)
-	cache.Put(3, 3)
-	cache.Put(4, 4)
-	cache.Get(4)
-	cache.Get(3)
-	cache.Get(2)
-	cache.Get(1)
-	cache.Put(5, 5)
-	cache.Get(1)
-	cache.Get(2)
-	cache.Get(3)
-	cache.Get(4)
-	cache.Get(5)
+	a := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
+	fmt.Println(leetcode.MaxArea(a))
 }
