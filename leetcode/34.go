13ba80b31320bbc106635d3209631017dcd28505
malishen <malishen@inke.cn>
2021-03-21 22:55:31 +0800

    34：寻找有序数组中目标的开始和结束位置

--- leetcode/34.go
+++ leetcode/34.go
@@ -8,22 +8,56 @@ package leetcode
 // 链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array
 // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
-// 时间O(n)
+// 递归
+// 时间O(lgN)
 // 空间O(1)
 func searchRange(nums []int, target int) []int {
-	l, r := 0, len(nums)-1
-	ret := []int{-1, -1}
-	for l < len(nums) && nums[l] != target {
-		l++
-	}
-	for r >= 0 && nums[r] != target {
-		r--
-	}
-	if l < len(nums) {
-		ret[0] = l
-	}
-	if r >= 0 {
-		ret[1] = r
-	}
-	return ret
+	l, r := seach(nums, 0, len(nums)-1, target)
+	return []int{l, r}
 }
+
+func seach(nums []int, l, r, target int) (int, int) {
+	if l > r {
+		return -1, -1
+	}
+	mid := l + (r-l)/2
+	if nums[mid] == target {
+		lIdx, rIdx := mid, mid
+		if isValidIndex(nums, mid-1) && nums[mid-1] == target {
+			lIdx, _ = seach(nums, l, mid-1, target)
+		}
+		if isValidIndex(nums, mid+1) && nums[mid+1] == target {
+			_, rIdx = seach(nums, mid+1, r, target)
+		}
+		return lIdx, rIdx
+	} else if nums[mid] > target {
+		return seach(nums, l, mid-1, target)
+	} else {
+		return seach(nums, mid+1, r, target)
+	}
+}
+
+func isValidIndex(nums []int, idx int) bool {
+	return idx >= 0 && idx < len(nums)
+}
+
+// 遍历法
+// 时间：O(n)
+// 空间O:(1)
+// func searchRange(nums []int, target int) []int {
+// 	l, r := 0, len(nums)-1
+// 	ret := []int{-1,-1}
+// 	for l < len(nums)&& nums[l] != target{
+// 		l++
+// 	}
+// 	for r >= 0 && nums[r] != target{
+// 		r--
+// 	}
+// 	if l < len(nums){
+// 		ret[0] = l
+// 	}
+// 	if r >= 0{
+// 		ret[1] = r
+// 	}
+// 	return ret
+// }
