a018c063132d7648a0051e2c7932f276fb264dab
herac1es <1045772673.com>
2021-03-25 20:18:48 +0800

    202快乐数

---
+++ leetcode/202_test.go
@@ -0,0 +1,34 @@
+package leetcode
+
+import "testing"
+
+func Test_isHappy(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			args: args{
+				n: 1,
+			},
+			want: true,
+		},
+		{
+			args: args{
+				n: 2,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isHappy(tt.args.n); got != tt.want {
+				t.Errorf("isHappy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
