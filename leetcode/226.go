4f00945d4d7490a3b4c703f7aec740e18fbd8a88
herac1es <1045772673.com>
2021-04-20 23:45:34 +0800

    226.翻转二叉树

---
+++ leetcode/226.go
@@ -0,0 +1,22 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// 226.翻转二叉树
+// 时间:O(n)
+// 空间:O(height)
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	invertTree(root.Left)
+	invertTree(root.Right)
+	root.Left, root.Right = root.Right, root.Left
+	return root
+}
