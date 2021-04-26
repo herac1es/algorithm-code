a34c42da0d2a550c1acfa5f3438b3c80d6c91b2c
herac1es <1045772673.com>
2021-04-26 11:18:44 +0800

    剑指offer54.62

---
+++ jzoffer/62.圆圈中最后剩下的数字.go
@@ -0,0 +1,28 @@
+package jzoffer
+
+// 数学推导法
+// f(n,m) = (f(n-1,m)+m)%n f(1,m) = 0
+
+func lastRemaining(n int, m int) int {
+
+	last := 0
+	for i := 1; i <= n; i++ {
+		last = (last + m) % i
+	}
+	return last
+}
+
+// 模拟：超时
+// func lastRemaining(n int, m int) int {
+//     queue := make([]int, n)
+//     for i := range queue {
+//         queue[i] = i
+//     }
+
+//     idx := 0
+//     for len(queue) > 1 {
+//         idx = (m-1+idx)%len(queue)
+//         queue = append(queue[:idx], queue[idx+1:]...)
+//     }
+//     return queue[0]
+// }
