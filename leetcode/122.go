d6662a93da1e45cddcdba1122b31a5494a08b460
heracles <malishen001@gmail.com>
2021-04-29 21:48:29 +0800

    fix:122.买卖股票的最佳时机II

--- leetcode/122.go
+++ leetcode/122.go
@@ -1,24 +1,16 @@
 package leetcode
 
-func MaxProfit122(prices []int) int {
-	n := len(prices)
-	dp1 := make([][2]int, n/2+1)
-	dp := make([][][2]int, n)
-	for i := 0; i < n; i++ {
-		dp[i] = dp1
-	}
-	for i := 0; i < n; i++ {
-		if i == 0 {
-			for k := 1; k <= n/2; k++ {
-				dp[i][k][0] = 0
-				dp[i][k][1] = -prices[i]
-			}
-			continue
-		}
-		for k := 1; k <= n/2; k++ {
-			dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1]+prices[i])
-			dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0]-prices[i])
+// 由于不限交易次数， 只要当天较前一天是涨，则买
+// 时间: O(n)
+// 空间: O(1)
+func maxProfitII(prices []int) int {
+	profit := 0
+	tmp := 0
+	for i := 1; i < len(prices); i++ {
+		tmp = prices[i] - prices[i-1]
+		if tmp > 0 {
+			profit += tmp
 		}
 	}
-	return dp[n-1][n/2][0]
+	return profit
 }
